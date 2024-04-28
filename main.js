e3d8f79a18bad92694612902c50431d45f764f5c
AAndersonSantos <andersonsantos.inc01@gmail.com>
2024-04-28 03:19:29 -0300

    Ajuste Navbar

--- main.js
+++ main.js
@@ -1,6 +1,19 @@
 const toggleButton = document.getElementsByClassName('toggle-button')[0]
 const navbarLinks = document.getElementsByClassName('navbar-links')[0]
+const navbar = document.getElementById("teste")
+
+let toggle = false
 
 toggleButton.addEventListener('click', () => {
   navbarLinks.classList.toggle('active')
+  
+  if(toggle == false){
+    toggle = true
+    navbar.style.backgroundColor = "#0000004d"
+    
+  } else {
+    toggle = false
+    navbar.style.backgroundColor = "transparent"
+  }
+
 })
